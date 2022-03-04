87389cd8f1ceb0ca0b33163cebe1e3417a2b6c25
Thrillonek <jindrich.kraina@gmail.com>
2022-03-04 16:19:12 +0100

    q

---
+++ Ghost Kid/Commands/fun/question.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: "question",
+    execute(message, args, cmd, client, discord, economy){
+
+        var embed = new discord.MessageEmbed()
+        .setTitle("Otázka")
+        .setDescription("Je Thrillonek dobrý developer?")
+
+        var row = new discord.MessageActionRow()
+        .addComponents([
+            new discord.MessageButton().setLabel("Ano"),
+            new discord.MessageButton().setLabel("Ne"),
+            new discord.MessageButton().setLabel("Možná")
+        ])
+    }
+}
